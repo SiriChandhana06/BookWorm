f7c738163b98c8bd41ea915b5805332cb8523e55
sirisiri3006@gmail.com <sirisiri3006@gmail.com>
2024-01-31 22:08:26 +0530

    updated the bookworm

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='bg'>
+      <Home/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+function Home(){
+    return(
+        <div className="home1"> 
+           <div className="h">
+            <h1 className="home">BOOK WORM...</h1>
+            </div>
+            <div className="btn">
+            <button className="odd">Login</button>
+            <button className="even">Signup</button>
+            </div>
+        </div>
+    )
+}
+export default Home;
