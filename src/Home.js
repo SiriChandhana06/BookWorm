f7fffd279528fc1b79cdf2291f2d0091b2bfa1e6
sirisiri3006@gmail.com <sirisiri3006@gmail.com>
2024-02-02 21:12:18 +0530

    react

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,19 @@
+
 function Home(){
+    function login(){
+        console.log("helo");
+    }
+    function signup(){
+        console.log("hi");
+    }
     return(
         <div className="home1"> 
            <div className="h">
             <h1 className="home">BOOK WORM...</h1>
             </div>
             <div className="btn">
-            <button className="odd">Login</button>
-            <button className="even">Signup</button>
+            <button className="odd" onClick={login}>Login</button>
+            <button className="even"onClick={signup}>Signup</button>
             </div>
         </div>
     )
---
+++ src/Login.js
@@ -0,0 +1,8 @@
+function loginpage(){
+    return(
+        <div className="loginpage">
+            <h1>LOGIN</h1>
+        </div>
+    )
+}
+export default loginpage;
